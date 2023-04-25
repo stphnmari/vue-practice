28c0de9c49732d14d6d841b4b37fa58a9c27a6f4
STEPHANIE_MARI <sfdmari76@gmail.com>
2023-04-25 19:20:26 +0800

    Laravel Todo List

---
+++ resources/views/todoList/show.blade.php
@@ -0,0 +1,29 @@
+@extends('todolist.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Lists</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('todolist.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $todo->description }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date:</strong>
+                {{ $todo->date }}
+            </div>
+        </div>
+    </div>
+@endsection
